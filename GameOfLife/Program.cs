ef551badc584a312dc8b1caedfe222af49a0d8ae
Mathias Beckius <mathiasbeckius@hotmail.com>
2024-05-09 23:08:35 +0200

    Cool pattern

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,12 +1,32 @@
-﻿var matrix = new CoreLib.CellMatrix(20, 20);
-matrix[10, 8] = CoreLib.Cell.CreateLiveCell();
-matrix[10, 9] = CoreLib.Cell.CreateLiveCell();
-matrix[9, 10] = CoreLib.Cell.CreateLiveCell();
-matrix[9, 11] = CoreLib.Cell.CreateLiveCell();
-matrix[11, 10] = CoreLib.Cell.CreateLiveCell();
-matrix[11, 11] = CoreLib.Cell.CreateLiveCell();
-matrix[10, 12] = CoreLib.Cell.CreateLiveCell();
-matrix[10, 13] = CoreLib.Cell.CreateLiveCell();
+﻿var matrix = new CoreLib.CellMatrix(35, 70);
+var offsetX = 15;
+matrix[0, 1 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[1, 2 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[2, 0 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[2, 1 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[2, 2 + offsetX] = CoreLib.Cell.CreateLiveCell();
+var offsetY = 3;
+matrix[offsetY + 10, 8] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 10, 9] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 9, 10] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 9, 11] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 11, 10] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 11, 11] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 10, 12] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 10, 13] = CoreLib.Cell.CreateLiveCell();
+offsetY = 10;
+offsetX = 10;
+matrix[offsetY + 15, 0 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 15, 1 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 15, 2 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 16, 3 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 17, 3 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 17, 4 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 17, 5 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 18, 3 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 19, 0 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 19, 1 + offsetX] = CoreLib.Cell.CreateLiveCell();
+matrix[offsetY + 19, 2 + offsetX] = CoreLib.Cell.CreateLiveCell();
 
 while (true)
 {
@@ -15,9 +35,14 @@ while (true)
     {
         var rowAsString = new System.Text.StringBuilder();
         foreach (var column in Enumerable.Range(0, matrix.ColumnCount))
-            rowAsString.Append(matrix[row, column].IsAlive ? "*" : " ");
+            rowAsString.Append(matrix[row, column].IsAlive ? '*' : ' ');
+        rowAsString.Append('|');
         Console.WriteLine(rowAsString.ToString());
     }
+    var lineRow = new System.Text.StringBuilder();
+    foreach (var column in Enumerable.Range(0, matrix.ColumnCount))
+        lineRow.Append('-');
+    Console.WriteLine(lineRow.ToString());
     Console.ReadLine();
     matrix = CoreLib.PatternGenerator.GenerateNewPattern(matrix);
 }
