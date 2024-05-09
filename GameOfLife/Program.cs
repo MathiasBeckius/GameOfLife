72b8fd4692e31a9b8640ab859af79d1056d6428b
Mathias Beckius <mathiasbeckius@hotmail.com>
2024-05-09 17:27:34 +0200

    Working automation (#3)

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,14 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var matrix = new CoreLib.CellMatrix(20, 20);
+matrix[10, 8] = CoreLib.Cell.CreateLiveCell();
+matrix[10, 9] = CoreLib.Cell.CreateLiveCell();
+matrix[9, 10] = CoreLib.Cell.CreateLiveCell();
+matrix[9, 11] = CoreLib.Cell.CreateLiveCell();
+matrix[11, 10] = CoreLib.Cell.CreateLiveCell();
+matrix[11, 11] = CoreLib.Cell.CreateLiveCell();
+matrix[10, 12] = CoreLib.Cell.CreateLiveCell();
+matrix[10, 13] = CoreLib.Cell.CreateLiveCell();
 
-var twoDimensionalArray = new CoreLib.CircularArray<CoreLib.CircularArray<int>>(3);
-for (var i = 0; i < twoDimensionalArray.Length; i++)
-    twoDimensionalArray[i] = new CoreLib.CircularArray<int>(3);
-twoDimensionalArray[0][0] = 0;
-twoDimensionalArray[1][0] = 0;
-twoDimensionalArray[2][0] = 0;
-
-var circularArray = new CoreLib.CircularArray<int>(3);
-Console.WriteLine($"circularArray[0]={circularArray[0]}");
-circularArray[0] = 1;
-Console.WriteLine($"circularArray[0]={circularArray[0]}");
+while (true)
+{
+    Console.Clear();
+    foreach (var row in Enumerable.Range(0, matrix.RowCount))
+    {
+        var rowAsString = new System.Text.StringBuilder();
+        foreach (var column in Enumerable.Range(0, matrix.ColumnCount))
+            rowAsString.Append(matrix[row, column].IsAlive ? "*" : " ");
+        Console.WriteLine(rowAsString.ToString());
+    }
+    Console.ReadLine();
+    matrix = CoreLib.PatternGenerator.GenerateNewPattern(matrix);
+}
